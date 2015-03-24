b1b4ffd8fceea514b5348bf0ce93865b1944cd5e
JohnWRRC <jw.ribeiro.rc@gmail.com>
2015-03-24 15:43:12 -0300

    Arrumei o query

--- split_buffers_in_lands.py
+++ split_buffers_in_lands.py
@@ -2,7 +2,7 @@ import os
 import numpy as np
 import arcpy
 from arcpy import env
-env.workspcae=r"C:\_data\vivi_brito\shps_500m\buffers_lands_split"
+env.workspace=r"C:\_data\vivi_brito\shps_500m\buffers_lands_split"
 os.chdir(r'C:\_data\vivi_brito\shps_500m')
 #tabela_lands=open("tabela_indentify_lands.txt",'rb')
 
@@ -20,7 +20,7 @@ for i in lista:
 
 for slct in lista_v02:
     output='buffer_500m_utmZ22s_v02_land'+slct
-    whereClause="ptos='%s'"%slct
+    whereClause="pto_amos='%s'"%slct
     arcpy.SelectLayerByAttribute_management('buffer_500m_utmZ22s_v02',"NEW_SELECTION",whereClause)
     arcpy.CopyFeatures_management('buffer_500m_utmZ22s_v02',output)
         
